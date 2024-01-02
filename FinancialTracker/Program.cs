1b2edd48a9124cf14afbb89cdfbd15f9c50ab345
sharpmalm <no@example.com>
2024-01-02 11:28:41 +0100

    created main menu with first switch case and moved income code to separate class

--- FinancialTracker/Program.cs
+++ FinancialTracker/Program.cs
@@ -3,63 +3,36 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "C:\\Users\\vsc\\source\\repos\\FinancialTracker\\FinancialTracker\\entries.txt");
-        FileHandler fileHandler = new FileHandler(fileName);
-        List<string> entries = fileHandler.ReadFileEntries();
-
-        try
+        bool exit = false;
+        while (!exit)
         {
-            Console.Write("Enter amount: ");
-            string amountString = Console.ReadLine();
-            if (amountString != null)
+            Console.WriteLine("Press 1 to enter income or 2 to exit!");
+            string input = Console.ReadLine();
+            switch (input)
             {
-                int amount = int.Parse(amountString);
+                case "1":
+                    IncomeHandler handler = new IncomeHandler();
+                    handler.ToString();
+                    break;
+                case "2":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
 
-                Console.Write("Enter date (yyyy-mm-dd): ");
-                string dateString = Console.ReadLine();
-                if (dateString != null)
-                {
-                    DateTime date = DateTime.Parse(dateString);
-
-                    Console.Write("Enter description: ");
-                    string description = Console.ReadLine();
-                    if (description != null)
-                    {
-                        Income income = new Income(amount, date, description);
-                        entries.Add(income.ToString());
-
-                        fileHandler.WriteToFile(entries);
-                        Console.WriteLine("Entry added successfully.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input format. Please enter a valid description.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input format. Please enter a valid date in yyyy-mm-dd format.");
-                }
             }
-            else
-            {
-                Console.WriteLine("Invalid input format. Please enter a valid integer for amount.");
-            }
+
         }
-        catch (FormatException ex)
-        {
-            Console.WriteLine("Invalid input format. Please enter a valid integer for amount and a valid date in yyyy-mm-dd format.");
-            Console.WriteLine(ex.Message);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("An error occurred while processing your request.");
-            Console.WriteLine(ex.Message);
-        }
+        //IncomeHandler handler = new IncomeHandler();
+        //handler.ToString();
     }
 }
+        
+  
--- FinancialTracker/UserInterface.cs
+++
@@ -1,6 +0,0 @@
-namespace FinancialTracker;
-
-class UserInterface
-{
-
-}
